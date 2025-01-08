3b2ddeb6734bfc1e3b569a6100034ea2f86fc48e
xiao <yangyuxiao1235@gmail.com>
2025-01-08 17:12:16 +0800

    去除部分冗余代码

--- libs/rag/server/chat/utils.py
+++ libs/rag/server/chat/utils.py
@@ -2,7 +2,6 @@ from typing import Dict, List, Union
 from rag.server.models.kb_spec import Context
 from rag.server.models.model_spec import History
 
-SYSTEM_PROMPT_INDEX = 0
 USER_PROMPT_INDEX = 1
 
 def construct_message(
@@ -16,7 +15,7 @@ def construct_message(
         "history": history,
         "contexts": contexts,
     }
-    prompt_template[SYSTEM_PROMPT_INDEX]["content"] = prompt_template[SYSTEM_PROMPT_INDEX]["content"].format(format_context)
+
     prompt_template[USER_PROMPT_INDEX]["content"] = prompt_template[USER_PROMPT_INDEX]["content"].format(format_context)
 
     return prompt_template
