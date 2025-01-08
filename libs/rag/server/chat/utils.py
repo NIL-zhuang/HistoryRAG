d819ca4201e6037a2ea445612f183bc5484b82a2
xiao <yangyuxiao1235@gmail.com>
2025-01-08 17:08:37 +0800

    优化PROMPT_TEMPLATE结构，使用black规范化json格式；重构construct_message方法代码，提高可读性

--- libs/rag/server/chat/utils.py
+++ libs/rag/server/chat/utils.py
@@ -2,7 +2,8 @@ from typing import Dict, List, Union
 from rag.server.models.kb_spec import Context
 from rag.server.models.model_spec import History
 
-
+SYSTEM_PROMPT_INDEX = 0
+USER_PROMPT_INDEX = 1
 
 def construct_message(
     query: str,
@@ -10,8 +11,12 @@ def construct_message(
     contexts: List[Context],
     prompt_template: Union[str, Dict[str, str], List[Dict[str, str]]]
 ) -> List[Dict[str, str]]:
-
-    prompt_template[0]["content"] = prompt_template[0]["content"].format(**locals())
-    prompt_template[1]["content"] = prompt_template[1]["content"].format(**locals())
+    format_context = {
+        "query": query,
+        "history": history,
+        "contexts": contexts,
+    }
+    prompt_template[SYSTEM_PROMPT_INDEX]["content"] = prompt_template[SYSTEM_PROMPT_INDEX]["content"].format(format_context)
+    prompt_template[USER_PROMPT_INDEX]["content"] = prompt_template[USER_PROMPT_INDEX]["content"].format(format_context)
 
     return prompt_template
